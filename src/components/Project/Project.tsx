9015213a7570c80263ec94c4f7d59b6a6be83861
Nishanth <113757661+nxshanth13@users.noreply.github.com>
2024-12-05 10:40:55 +0530

    Update Project.tsx

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -126,12 +126,12 @@ export function Project() {
                 <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
               </svg>
               <div className="project-links">
-                <a href="https://github.com/CodeVinayak/Real-Time-Hand-Gesture-Driven-3D-Object-Manipulation" target="_blank" rel="noreferrer">
+{/*                 <a href="https://github.com/CodeVinayak/Real-Time-Hand-Gesture-Driven-3D-Object-Manipulation" target="_blank" rel="noreferrer">
                   <img src={githubIcon} alt="Visit site" />
                 </a>
                 <a href="https://youtu.be/NOm-3MynPLE" target="_blank" rel="noreferrer">
                   <img src={externalLink} alt="Visit site" />
-                </a>
+                </a> */}
               </div>
             </header>
             <div className="body">
@@ -155,9 +155,9 @@ export function Project() {
             <header>
               <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
               <div className="project-links">
-                <a href="https://velocityai.vinayaksingh.com/" target="_blank" rel="noreferrer">
+{/*                 <a href="https://velocityai.vinayaksingh.com/" target="_blank" rel="noreferrer">
                   <img src={externalLink} alt="Visit site" />
-                </a>
+                </a> */}
               </div>
             </header>
             <div className="body">
